0ff6e57badb469563d649f47a7aaae15ce2e0c09
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-02-22 21:32:07 +0200

    :hammer: Add Netty initialization boilerplate

---
+++ src/main/java/NettyHTTP/NettyHTTPServer.java
@@ -0,0 +1,35 @@
+package NettyHTTP;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class NettyHTTPServer {
+    public static void start(int port) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new HTTPServerInitializer());
+            Channel ch = b.bind(port).sync().channel();
+            System.err.println("Server is listening on http://127.0.0.1:" + port + '/');
+            ch.closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        NettyHTTPServer.start(8080);
+    }
+}
